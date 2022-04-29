d3a48270530d82ab8cddffb51b9ed4974524948c
takwai <takwai0318@gmail.com>
2022-04-29 17:57:31 -0400

    change to animes

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Car
+from .models import Anime
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Define the home view
@@ -33,25 +33,25 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-def cars_index(request):
-    cars = Car.objects.filter(user=request.user)
-    return render(request, 'cars/index.html', {'cars': cars})
+def animes_index(request):
+    animes = Anime.objects.filter(user=request.user)
+    return render(request, 'animes/index.html', {'animes': animes})
 
-def cars_detail(request, car_id):
-    car = Car.objects.get(id=car_id)
-    return render(request, 'cars/detail.html', {'car': car})
+def animes_detail(request, anime_id):
+    anime = Anime.objects.get(id=anime_id)
+    return render(request, 'animes/detail.html', {'anime': anime})
 
-class CarCreate(CreateView):
-  model = Car
-  fields =['make','model','year','color']
+class animeCreate(CreateView):
+  model = Anime
+  fields =['title','category','language','description']
   def form_valid(self, form):
-    form.instance.user = self.request.user  # form.instance is the car
+    form.instance.user = self.request.user  # form.instance is the anime
     return super().form_valid(form)
 
-class CarUpdate(UpdateView):
-  model = Car
-  fields =['make','model','year','color']
+class animeUpdate(UpdateView):
+  model = Anime
+  fields =['category','language','description']
   
-class CarDelete(DeleteView):
-  model = Car
-  success_url = '/cars/'
+class animeDelete(DeleteView):
+  model = Anime
+  success_url = '/animes/'
