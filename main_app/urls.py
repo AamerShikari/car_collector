e83e161c05a7777ad31057887405fb44fff67b64
takwai <takwai0318@gmail.com>
2022-04-29 12:21:28 -0400

    added cars index.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'), #example path
 	path('about/', views.about, name='about'),
+	path('cars/', views.cars_index, name='index'),
 	path('accounts/', include('django.contrib.auth.urls')),
 	path('accounts/signup/', views.signup, name='signup'),
 ]
