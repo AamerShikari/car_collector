f5770bd2ceda297b575767ed1116f772c1eb4ed4
AamerShikari <shikariaamer@gmail.com>
2022-04-29 02:23:50 -0400

    Authentication of User, Authorization of User

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'), #example path
+	path('about/', views.about, name='about'),
+	path('accounts/', include('django.contrib.auth.urls')),
+	path('accounts/signup/', views.signup, name='signup'),
 ]
